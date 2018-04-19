6a8bff983c4c4aa97ca343a52298693c1c3b21e2
Rik van Duijn <ik@rikvanduijn.nl>
2018-04-19 11:47:51 +0200

    Add simple touch id authentication

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.authenticate = this.authenticate.bind(this)
+
+    this.state = {
+      authenticated: false,
+    }
+  }
+
+  async authenticate() {
+    const fingerPrintResult = await Expo.Fingerprint.authenticateAsync('Your notes are safe. Unlock them with your fingerprint')
+    if (fingerPrintResult.success) {
+      this.setState({ authenticated: true })
+    } else {
+      if (fingerPrintResult.error === "user_fallback") {
+        // Handle manual login
+      }
+    }
+  }
+
+  logOut = () => {
+    this.setState({ authenticated: false })
+  }
+
+  renderTouchIdAuth = () => {
+    return(
+      <View>
+        <Button title="Log in with Touch ID" onPress={this.authenticate} />
+      </View>
+    )
+  }
+
+  renderLogin = () => {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {
+          Expo.Fingerprint.hasHardwareAsync() && Expo.Fingerprint.isEnrolledAsync()
+          ? this.renderTouchIdAuth()
+          : <Text>This app only works on devices with Touch ID.</Text>
+        }
       </View>
     );
+  }
+
+  render() {
+    if (!this.state.authenticated) { return this.renderLogin() }
+
+    return (
+      <View style={styles.container}>
+        <Text>You are logged in!</Text>
+        <Button title="Log out" onPress={this.logOut} />
+      </View>
+    )
   }
 }
 
